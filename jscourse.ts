15663c8c97cd5487282915f0126d7191429fe876
Milosz Czapla <totmelek@gmail.com>
2021-05-26 20:29:19 +0200

    Added config map + String basics

--- jscourse.ts
+++ jscourse.ts
@@ -1,6 +1,29 @@
-let age: number, year: number;
+// string
+console.log('"hello " worls')
 
-age = 25;
-year = 1500;
-console.log(age, year);
+let fullName: string, firstName: string, lastName: string, mail: string;
 
+mail = "hello@happy.mail.com";
+
+firstName = "Leaon";
+lastName = "Geopard";
+
+fullName = firstName + " " + lastName;
+console.log(fullName);
+
+console.log("pozycja 3 ",fullName[2]);
+
+// String methods
+console.log(fullName.length);
+
+console.log(fullName.toUpperCase());
+
+console.log(fullName[0].toLowerCase() );
+
+let lowerCasedName: string = fullName.toLowerCase();
+
+console.log(lowerCasedName);
+
+let index: number = mail.indexOf(".");
+
+console.log(index);
