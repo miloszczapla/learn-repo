62176ca455801c726a8045437d4ed3f7a99b0dd2
Milosz Czapla <totmelek@gmail.com>
2021-05-27 18:36:14 +0200

    Added file with function

---
+++ fun.js
@@ -0,0 +1,9 @@
+"use strict";
+function Log() {
+    console.log('It is fun!');
+}
+const expressedFunction = function () {
+    console.log('expressing...');
+};
+Log();
+expressedFunction();
---
+++ fun.ts
@@ -0,0 +1,11 @@
+function Log() {
+    console.log('It is fun!');
+}
+
+const expressedFunction = function () {
+    console.log('expressing...');
+};
+
+Log();
+
+expressedFunction();
